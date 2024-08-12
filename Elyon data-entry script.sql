f636e7dcf5c1e58c372cd7f26ca7d34676bd300d
Elijah Okpako <160936178+Jay0494@users.noreply.github.com>
2024-08-12 22:35:51 +0100

    Add files via upload

---
+++ Elyon data-entry script.sql
@@ -0,0 +1,52 @@
+SELECT * 
+	FROM purchase;
+    
+INSERT INTO 
+	purchase (Id, Buyer_Id, Item, Item_category, Service_provider, Price, Quantity, `Date`)
+	VALUES(36, 'E2', 'Rolls', 'Snacks', 'ASDA', 2.90, 1, '2024-08-07'),
+          (37, 'E2', 'Soft_drinks', 'Groceries', 'ASDA', 0.95, 1, '2024-08-07'),
+          (38, 'E2', 'Spaghetti', 'Groceries', 'ASDA', 0.28, 2, '2024-08-07'),
+          (39, 'E2', 'Rice', 'Groceries', 'ASDA', 2.40, 2, '2024-08-07'),
+          (40, 'E2', 'Baby_formula', 'Baby_food', 'ASDA', 9.75, 1, '2024-08-07'),
+          (41, 'E2', 'Sugar', 'Groceries', 'ASDA', 0.89, 1, '2024-08-07'),
+          (42, 'E2', 'Milk', 'Groceries', 'ASDA', 2.15, 1, '2024-08-07'),
+          (43, 'E2', 'Low_fat_yogurt', 'Groceries', 'LIDL', 0.85, 3, '2024-08-01'),
+          (44, 'E2', 'Medium_loaf_white_bread', 'Groceries', 'LIDL', 0.47, 2, '2024-08-01'),
+          (45, 'E2', 'Medium_loaf_wholemeal_bread', 'Groceries', 'LIDL', 0.47, 2, '2024-08-01'),
+          (46, 'E2', 'Unwashed_spinach', 'Groceries', 'LIDL', 0.99, 1, '2024-08-01'),
+          (47, 'E2', 'Red_onions', 'Groceries', 'LIDL', 0.95, 1, '2024-08-01'),
+          (48, 'E2', 'Custard', 'Groceries', 'LIDL', 0.69, 12, '2024-08-01'),
+          (49, 'E2', 'Evaporated_milk', 'Groceries', 'LIDL', 0.59, 1, '2024-08-01'),
+          (50, 'E2', 'Chocolate_orange_cream', 'Groceries', 'LIDL', 0.49, 2, '2024-08-01'),
+          (51, 'E1', 'Electricity', 'Utility', 'Utilita', 10, 1, '2024-08-04'),
+          (52, 'E1', 'Electricity', 'Utility', 'Utilita', 5, 1, '2024-08-07'),
+          (53, 'E1', 'Electricity', 'Utility', 'Utilita', 5, 1, '2024-08-09');
+          
+          
+-- data entry
+INSERT INTO 
+	purchase (Id, Buyer_Id, Item, Item_category, Service_provider, Price, Quantity, `Date`)
+    VALUES(54, 'E1', 'Self_raising_flour', 'Groceries', 'LIDL', 0.79, 1, '2024-08-12'),
+		  (55, 'E1', 'Low_fat_yogurt', 'Groceries', 'LIDL', 0.85, 3, '2024-08-12'),
+          (56, 'E1', 'Custard', 'Groceries', 'LIDL', 0.69, 6, '2024-08-12'),
+          (57, 'E1', 'Choc_milkshake_mix', 'Groceries', 'LIDL', 1.99, 1, '2024-08-12'),
+          (58, 'E1', 'Evaporated_Milk', 'Groceries', 'LIDL', 0.59, 1, '2024-08-12'),
+          (59, 'E1', 'Bananas', 'Groceries', 'LIDL', 0.78, 2, '2024-08-12'),
+          (60, 'E1', 'Medium_loaf_wholemeal', 'Groceries', 'LIDL', 0.47, 1, '2024-08-12'),
+          (61, 'E1', 'NF_Sale_item', 'Groceries', 'LIDL', 3, 1, '2024-08-12'),
+          (62, 'E1', 'Chick_drumsticks', 'Groceries', 'LIDL', 1.99, 1, '2024-08-12'),
+          (63, 'E1', 'Red_onions', 'Groceries', 'LIDL', 0.95, 1, '2024-08-12'),
+          (64, 'E1', 'Medium_loaf_white', 'Groceries', 'LIDL', 0.47, 2, '2024-08-12'),
+          (65, 'E1', 'Jam_filled_doughnut', 'Groceries', 'LIDL', 0.39, 1, '2024-08-12'),
+          (66, 'E1', 'Granulated_sugar', 'Groceries', 'LIDL', 1.09, 1, '2024-08-12'),
+          (67, 'E1', 'Rolls', 'Groceries', 'ASDA', 2.90, 2, '2024-08-12'),
+          (68, 'E1', 'Eggs', 'Groceries', 'ASDA', 1.99, 2, '2024-08-12'),
+          (69, 'E1', 'Tomatoes', 'Groceries', 'ASDA', 1.80, 1, '2024-08-12'),
+          (70, 'E1', 'Wipes', 'Baby_stuff',	'ASDA', 0.65, 2, '2024-08-12'),
+          (71, 'E1', 'SIM', 'Extra_stuff', 'ASDA', 1.00, 1, '2024-08-12'),
+          (72, 'E1', 'Wholemilk', 'Groceries', 'ASDA', 2.15, 1, '2024-08-12'),
+          (73, 'E1', 'Nappies', 'Babystuff', 'ASDA', 2.89, 1, '2024-08-12');
+          
+          
+          
+	
